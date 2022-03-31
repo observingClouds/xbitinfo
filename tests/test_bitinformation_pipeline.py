c0997285ca58372c7450e2dbaa807e412171eb30
Aaron Spring <aaronspring@users.noreply.github.com>
2022-03-31 21:41:59 +0200

    Update test_bitinformation_pipeline.py

--- tests/test_bitinformation_pipeline.py
+++ tests/test_bitinformation_pipeline.py
@@ -2,7 +2,6 @@
 
 """Tests for `bitinformation_pipeline` package."""
 
-import pytest
 import xarray as xr
 
 from bitinformation_pipeline import bitinformation_pipeline as bm
@@ -12,3 +11,4 @@ def test_get_bitinformation():
     """Test bm.get_bitinformation."""
     ds = xr.tutorial.load_dataset("rasm")
     bitinfo = bm.get_bitinformation(ds, dim=1)
+    assert bitinfo
