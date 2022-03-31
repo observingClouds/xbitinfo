4258ac3e8ce0b4755daca75d87ceb88b76e0475f
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2022-03-31 16:04:09 +0200

    wrap get_keepbits and get_bitinformation

--- bitinformation_pipeline/bitinformation_pipeline.py
+++ bitinformation_pipeline/bitinformation_pipeline.py
@@ -1,3 +1,5 @@
+import argparse
+
 from julia.api import Julia
 
 jl = Julia(compiled_modules=False)
@@ -7,8 +9,49 @@ jl.eval('import Pkg; Pkg.add("BitInformation"); Pkg.add("NetCDF");')
 jl.eval('Pkg.add("PyPlot"), Pkg.add("StatsBase"); Pkg.add("ColorSchemes");')
 jl.using("BitInformation")
 
-jl.eval('include("get_n_plot_bitinformation.jl")')
-Main.inputfile = "EUREC4A_DOM01_radiation_20200122T180000Z_latlon.nc"
-bitinformation = jl.eval("get_bitinformation(inputfile)")
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "filename",
+    help="filename of dataset (netCDF-file) whose information content should"
+    " be retrieved",
+    type=str,
+)
+args = parser.parse_args()
 
-print(bitinformation)
+jl.eval('include("get_n_plot_bitinformation.jl")')
+
+
+def get_bitinformation(filename, dim=None, mask=None, label=None):
+    """Wrapper around BitInformation.jl
+
+    Returns
+    -------
+    info_per_bit : dict
+      Information content per bit and variable
+    """
+    Main.inputfile = filename
+    info_per_bit = jl.eval("get_bitinformation(inputfile)")
+    return info_per_bit
+
+
+def get_keepbits(info_per_bit, information_content=0.99):
+    """Get the amount of bits to keep for a given information content
+
+    Returns
+    -------
+    keepbits : dict
+      Number of bits to keep per variable
+    """
+    Main.info_per_bit = info_per_bit
+    Main.information_content = information_content
+    keepbits = jl.eval("get_keepbits(info_per_bit, information_content)")
+    return keepbits
+
+
+if __name__ == "__main__":
+    info_per_bit = get_bitinformation(
+        "EUREC4A_DOM01_radiation_20200122T180000Z_latlon.nc"
+    )
+    print(info_per_bit)
+    keepbits = get_keepbits(info_per_bit)
+    print(keepbits)
--- bitinformation_pipeline/get_n_plot_bitinformation.jl
+++ bitinformation_pipeline/get_n_plot_bitinformation.jl
@@ -1,4 +1,4 @@
-using NetCDF, PyPlot, BitInformation, LaTeXStrings
+using NetCDF, PyPlot, BitInformation, LaTeXStrings, JSON
 using Statistics, StatsBase, ColorSchemes, Printf, PyPlot
 
 function get_bitinformation(filename::String)
@@ -19,7 +19,7 @@ function get_bitinformation(filename::String)
 
     n = length(varnames)
     IC = fill(0.0,n,nbits)
-
+    bitinfo_dict = Dict()
     for (i,var) in enumerate(varnames)
         if length(size(ncfile.vars[var])) == 3
             X = ncfile.vars[var][:,1,:]
@@ -30,10 +30,53 @@ function get_bitinformation(filename::String)
         end
         BitInformation.signed_exponent!(X)
         IC[i,:] = bitinformation(X,dim=1)
-        print(var)
+        bitinfo_dict[var] = IC[i,:]
+        print("Processing: ", var, "\n")
     end
 
-    return IC
+    #print(json(bitinfo_dict,4))
+
+    return bitinfo_dict
+
+end
+
+
+function get_keepbits(bitinfo_dict, inflevel=0.99)
+    # filter insignificant information via binomial distribution as explained in methods
+    # for some variables the last bits do not have a free entropy of 1 bit, in that case
+    # adjust the threshold by the information in the very last bits (which should always be 0)
+    nvars = length(bitinfo_dict)
+    varnames = keys(bitinfo_dict)
+
+    ICfilt = copy(reduce(vcat, transpose.(values(bitinfo_dict))))
+    for i in 1:nvars
+        ic = ICfilt[i,:]
+        p = BitInformation.binom_confidence(900*440*137,0.99)  # get chance p for 1 (or 0) from binom distr
+        M₀ = 1 - entropy([p,1-p],2)                            # free entropy of random 50/50 at trial size
+        threshold = max(M₀,1.5*maximum(ic[end-3:end]))         # in case the information never drops to zero
+                                                               # use something a bit bigger than maximum
+                                                               # of the last 4 bits
+        insigni = (ic .<= threshold) .& (collect(1:length(ic)) .> 9)
+        ICfilt[i,insigni] .= floatmin(Float64)
+    end
+
+
+    # find bits with 99/100% of information
+    ICcsum = cumsum(ICfilt,dims=2)
+    ICcsum_norm = copy(ICcsum)
+    for i in 1:nvars
+        ICcsum_norm[i,:] ./= ICcsum_norm[i,end]
+    end
+
+    infbits_dict = Dict()
+    for (i,var) in enumerate(varnames)
+        infbits_dict[var] = argmax(ICcsum_norm[i,:] .> inflevel)
+    end
+
+    #infbits = [argmax(ICcsum_norm[i,:] .> inflevel) for i in 1:nvars]
+    #infbits100 = [argmax(ICcsum_norm[i,:] .> 0.999999999) for i in 1:nvars];
+
+    return infbits_dict
 
 end
 
@@ -195,4 +238,7 @@ function main(filename::String)
 
 end
 
-main("EUREC4A_DOM01_radiation_20200122T180000Z_latlon.nc")
+
+#bitinfo_dict = get_bitinformation("EUREC4A_DOM01_radiation_20200122T180000Z_latlon.nc")
+#keepbits_dict = get_keepbits(bitinfo_dict, 0.99)
+#print(json(keepbits_dict,4))
