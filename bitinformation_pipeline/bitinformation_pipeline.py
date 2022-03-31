6cc39add0e107b2cb55c27341e52ce3abdcbed02
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2022-03-31 16:11:16 +0200

    move argument parser to __main__

--- bitinformation_pipeline/bitinformation_pipeline.py
+++ bitinformation_pipeline/bitinformation_pipeline.py
@@ -8,17 +8,19 @@ from julia import Main  # noqa: E402
 jl.eval('import Pkg; Pkg.add("BitInformation"); Pkg.add("NetCDF");')
 jl.eval('Pkg.add("PyPlot"), Pkg.add("StatsBase"); Pkg.add("ColorSchemes");')
 jl.using("BitInformation")
-
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "filename",
-    help="filename of dataset (netCDF-file) whose information content should"
-    " be retrieved",
-    type=str,
-)
-args = parser.parse_args()
-
 jl.eval('include("get_n_plot_bitinformation.jl")')
+
+
+def get_user_input():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "filename",
+        help="filename of dataset (netCDF-file) whose information "
+        "content should be retrieved",
+        type=str,
+    )
+    args = parser.parse_args()
+    return args
 
 
 def get_bitinformation(filename, dim=None, mask=None, label=None):
@@ -49,9 +51,8 @@ def get_keepbits(info_per_bit, information_content=0.99):
 
 
 if __name__ == "__main__":
-    info_per_bit = get_bitinformation(
-        "EUREC4A_DOM01_radiation_20200122T180000Z_latlon.nc"
-    )
+    args = get_user_input()
+    info_per_bit = get_bitinformation(args.filename)
     print(info_per_bit)
     keepbits = get_keepbits(info_per_bit)
     print(keepbits)
