b5150634e9256e6e1f1577e18b8dcb57ba11b4b0
Aaron Spring <aaronspring@users.noreply.github.com>
2022-04-04 16:44:32 +0200

    Update bitinformation_pipeline.py

--- bitinformation_pipeline/bitinformation_pipeline.py
+++ bitinformation_pipeline/bitinformation_pipeline.py
@@ -3,9 +3,13 @@ from julia.api import Julia
 jl = Julia(compiled_modules=False, debug=True)
 from julia import Main  # noqa: E402
 
-jl.eval('import Pkg; Pkg.add("BitInformation"); Pkg.add("NetCDF");')
-jl.eval('Pkg.add("PyPlot"), Pkg.add("StatsBase"); Pkg.add("ColorSchemes");')
-jl.using("BitInformation")
+path_to_julia_functions = os.path.join(
+    os.path.dirname(__file__), "get_n_plot_bitinformation.jl"
+)
+Main.path = path_to_julia_functions
+jl.eval(
+    'import Pkg; Pkg.add(["BitInformation", "NetCDF", "PyPlot", "StatsBase", "ColorSchemes"])'
+)jl.using("BitInformation")
 
 jl.eval('include("get_n_plot_bitinformation.jl")')
 Main.inputfile = "EUREC4A_DOM01_radiation_20200122T180000Z_latlon.nc"
