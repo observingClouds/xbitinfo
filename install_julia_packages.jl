cb02148b393bdb33dcace58b880ee9b7c591778d
Aaron Spring <aaronspring@users.noreply.github.com>
2022-04-04 22:01:22 +0200

    Update install_julia_packages.jl

--- install_julia_packages.jl
+++ install_julia_packages.jl
@@ -1,4 +1,7 @@
 import Pkg
-Pkg.add(Pkg.PackageSpec(;name="NetCDF"))
-Pkg.add(["PyCall","PyPlot","Statistics","StatsBase", "JSON3", "LaTeXStrings", "JSON","BitInformation"])
+#Pkg.add(["PyCall","PyPlot","Statistics","StatsBase", "JSON3", "LaTeXStrings", "JSON","BitInformation"])
+Pkg.add("PyCall")
+Pkg.add("BitInformation")
+Pkg.add("Statistics")
+Pkg.add("StatsBase")
 Pkg.status()
