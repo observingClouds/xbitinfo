0f188ab9335e434fbc01d7a53a3bd419a0c26c3e
Aaron Spring <aaronspring@users.noreply.github.com>
2022-04-04 17:54:01 +0200

    Update install_julia_packages.jl

--- install_julia_packages.jl
+++ install_julia_packages.jl
@@ -1,3 +1,4 @@
 import Pkg
-Pkg.add(["PyCall","PyPlot","Statistics","StatsBase","NetCDF", "JSON3", "LaTXStrings", "JSON","BitInformation"])
+Pkg.add(["PyCall","PyPlot","Statistics","StatsBase", "JSON3", "LaTXStrings", "JSON","BitInformation"])
+Pkg.add(Pkg.PackageSpec(;name="NetCDF", version="0.11.3"))
 Pkg.status()
