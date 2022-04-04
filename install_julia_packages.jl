bdea88e050f6e4742994b27ae03901c86bd98a7d
Aaron Spring <aaronspring@users.noreply.github.com>
2022-04-04 22:02:23 +0200

    Update install_julia_packages.jl

--- install_julia_packages.jl
+++ install_julia_packages.jl
@@ -1,6 +1,7 @@
 import Pkg
 #Pkg.add(["PyCall","PyPlot","Statistics","StatsBase", "JSON3", "LaTeXStrings", "JSON","BitInformation"])
 Pkg.add("PyCall")
+Pkg.add("JSON")
 Pkg.add("BitInformation")
 Pkg.add("Statistics")
 Pkg.add("StatsBase")
