1d3723665d3bdef223927dfac4794d926fab98a8
Aaron Spring <aaronspring@users.noreply.github.com>
2022-04-04 17:27:57 +0200

    Update __init__.py

--- bitinformation_pipeline/__init__.py
+++ bitinformation_pipeline/__init__.py
@@ -4,4 +4,4 @@ __author__ = """Hauke Schulz"""
 __email__ = "hauke.schulz@mpimet.mpg.de"
 __version__ = "0.0.1"
 
-from .bitinformation_pipeline import get_bitinformation(
+from .bitinformation_pipeline import get_bitinformation
