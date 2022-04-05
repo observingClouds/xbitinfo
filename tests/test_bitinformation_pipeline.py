4f42e377c1de890601592d3b273924c7fe6d9f2c
Aaron Spring <aaronspring@users.noreply.github.com>
2022-04-05 18:02:27 +0200

    Update test_bitinformation_pipeline.py

--- tests/test_bitinformation_pipeline.py
+++ tests/test_bitinformation_pipeline.py
@@ -13,3 +13,11 @@ def test_get_bitinformation():
     bitinfo = bp.get_bitinformation(ds, dim=1)
     print(bitinfo)
     assert bitinfo
+
+def test_get_bitinformation_dim():
+    """Test bm.get_bitinformation is sensitive to dim."""
+    ds = xr.tutorial.load_dataset("rasm")
+    bitinfo1 = bp.get_bitinformation(ds, dim=1)
+    bitinfo3 = bp.get_bitinformation(ds, dim=3)
+    for v in bitinfo.keys():
+        bitinfo1[v]!=bitinfo3[v]
