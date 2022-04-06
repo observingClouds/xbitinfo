c14f2e7ba475d64d7dfc729241e706f4d6be088f
Aaron Spring <aaronspring@users.noreply.github.com>
2022-04-06 13:24:16 +0200

    Update __init__.py

--- bitinformation_pipeline/__init__.py
+++ bitinformation_pipeline/__init__.py
@@ -1,9 +1,5 @@
 """Top-level package for bitinformation_pipeline."""
 
-__author__ = """Hauke Schulz"""
-__email__ = "hauke.schulz@mpimet.mpg.de"
-__version__ = "0.0.1"
-
 from .bitinformation_pipeline import (
     get_bitinformation,
     get_keepbits,
