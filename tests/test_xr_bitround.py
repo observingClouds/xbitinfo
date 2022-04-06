304a3a2965fd3e28dfc0a06c4f8651c1a76a48fd
AS <aaron.spring@mpimet.mpg.de>
2022-04-06 15:48:31 +0200

    fix tests

--- tests/test_xr_bitround.py
+++ tests/test_xr_bitround.py
@@ -1,36 +1,23 @@
+import pytest
 import xarray as xr
 from xarray.testing import assert_allclose
 
 import bitinformation_pipeline as bp
 
 
-def test_xr_bitround(rasm):
-    """Test xr_bitround."""
+@pytest.mark.parametrize("keepbits", ["dict", "int"])
+def test_xr_bitround(rasm, keepbits):
+    """Test xr_bitround to different keepbits of type dict or int."""
     ds = rasm
     i = 15
-    keepbits = {v: i for v in ds.data_vars}
+    if keepbits == "dict":
+        keepbits = {v: i for v in ds.data_vars}
+    elif keepbits == "int":
+        keepbits = i
     ds_bitrounded = bp.xr_bitround(ds, keepbits)
-    assert_allclose(ds, ds_bitrounded)
+    assert_allclose(ds, ds_bitrounded, atol=0.01, rtol=0.01)
     for v in ds.data_vars:
         # attrs set
         assert ds_bitrounded[v].attrs["bitround_keepbits"] == i
         # different after bitrounding
         assert ((ds[v] - ds_bitrounded[v]) != 0).all()
-        # but close
-        assert_allclose(ds, ds_bitrounded, atol=0.01, rtol=0.01)
-
-
-def test_xr_bitround_keepbits_int(rasm):
-    """Test xr_bitround with keepbits as int."""
-    ds = rasm
-    i = 15
-    keepbits = i
-    ds_bitrounded = bp.xr_bitround(ds, keepbits)
-    assert_allclose(ds, ds_bitrounded)
-    for v in ds.data_vars:
-        # attrs set
-        assert ds_bitrounded[v].attrs["bitround_keepbits"] == i
-        # different after bitrounding
-        assert ((ds[v] - ds_bitrounded[v]) != 0).all()
-        # but close
-        assert_allclose(ds, ds_bitrounded, atol=0.01, rtol=0.01)
