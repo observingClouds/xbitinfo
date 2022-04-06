8871a67ae6d4d77840245df9819e6c82b91bcfe8
Aaron Spring <aaronspring@users.noreply.github.com>
2022-04-06 11:45:47 +0200

    fix tests

--- bitinformation_pipeline/get_n_plot_bitinformation.jl
+++ bitinformation_pipeline/get_n_plot_bitinformation.jl
@@ -20,31 +20,42 @@ function get_bitinformation(X::AbstractArray{T}; kwargs...) where {T<:Union{Int1
 end
 
 
-function get_keepbits(bitinfo_dict)
+
+function get_keepbits(bitinfo_dict, inflevel=0.99)
     # filter insignificant information via binomial distribution as explained in methods
     # for some variables the last bits do not have a free entropy of 1 bit, in that case
     # adjust the threshold by the information in the very last bits (which should always be 0)
-    bitinfo = bitinfo_dict["bitinfo"]
-    maskinfo = bitinfo_dict["maskinfo"]
-    inflevel = bitinfo_dict["inflevel"]
+    nvars = length(bitinfo_dict)
+    varnames = keys(bitinfo_dict)
 
-    ic = values(bitinfo)
-    p = BitInformation.binom_confidence(maskinfo,0.99)  # get chance p for 1 (or 0) from binom distr
-    M₀ = 1 - entropy([p,1-p],2)                            # free entropy of random 50/50 at trial size
-    threshold = max(M₀,1.5*maximum(ic[end-3:end]))         # in case the information never drops to zero
+    ICfilt = copy(reduce(vcat, transpose.(values(bitinfo_dict))))
+    for i in 1:nvars
+        ic = ICfilt[i,:]
+        p = BitInformation.binom_confidence(900*440*137,0.99)  # get chance p for 1 (or 0) from binom distr
+        M₀ = 1 - entropy([p,1-p],2)                            # free entropy of random 50/50 at trial size
+        threshold = max(M₀,1.5*maximum(ic[end-3:end]))         # in case the information never drops to zero
                                                                # use something a bit bigger than maximum
                                                                # of the last 4 bits
-    insigni = (ic .<= threshold) .& (collect(1:length(ic)) .> 9)
-    ic[insigni] .= floatmin(Float64)
+        insigni = (ic .<= threshold) .& (collect(1:length(ic)) .> 9)
+        ICfilt[i,insigni] .= floatmin(Float64)
+    end
+
 
     # find bits with 99/100% of information
-    ICcsum = cumsum(ic)
+    ICcsum = cumsum(ICfilt,dims=2)
     ICcsum_norm = copy(ICcsum)
-    ICcsum_norm[:] ./= ICcsum_norm[end]
+    for i in 1:nvars
+        ICcsum_norm[i,:] ./= ICcsum_norm[i,end]
+    end
 
-    infbits = argmax(ICcsum_norm[:] .> inflevel)
+    infbits_dict = Dict()
+    for (i,var) in enumerate(varnames)
+        infbits_dict[var] = argmax(ICcsum_norm[i,:] .> inflevel)
+    end
+
+    #infbits = [argmax(ICcsum_norm[i,:] .> inflevel) for i in 1:nvars]
     #infbits100 = [argmax(ICcsum_norm[i,:] .> 0.999999999) for i in 1:nvars];
 
-    return infbits
+    return infbits_dict
 
 end
