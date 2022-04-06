28c38306e30fe60323b1d58d954a4721fb96bf63
Aaron Spring <aaronspring@users.noreply.github.com>
2022-04-06 12:35:57 +0200

    fix notebook

--- bitinformation_pipeline/bitinformation_pipeline.py
+++ bitinformation_pipeline/bitinformation_pipeline.py
@@ -78,9 +78,8 @@ def get_bitinformation(ds, label=None, overwrite=False, **kwargs):
                 kwargs[
                     "masked_value"
                 ] = f"convert({str(ds[var].dtype).capitalize()},NaN)"
-            kwargs_str = " ," + ", ".join([f"{k}={v}" for k, v in kwargs.items()])
+            kwargs_str = ", " + ", ".join([f"{k}={v}" for k, v in kwargs.items()])
             kwargs_str = kwargs_str.replace("True", "true").replace("False", "false")
-            print(f"get_bitinformation(X{kwargs_str})")
             logging.debug(f"get_bitinformation(X{kwargs_str})")
             info_per_bit[var] = jl.eval(f"get_bitinformation(X{kwargs_str})")
         with open(label + ".json", "w") as f:
