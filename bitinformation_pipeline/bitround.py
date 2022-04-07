4dcb4da54daa780d564bac25d83a7e00aa6aa461
AS <aaron.spring@mpimet.mpg.de>
2022-04-07 08:57:11 +0200

    refactor xr_bitround

--- bitinformation_pipeline/bitround.py
+++ bitinformation_pipeline/bitround.py
@@ -10,19 +10,19 @@ def bitround(data, keepbits):
     return codec.decode(encoded)
 
 
-def xr_bitround(ds, keepbits):
+def xr_bitround(da, keepbits):
     """Apply bitrounding based on keepbits from bp.get_keepbits for xarray.Dataset or xr.DataArray.
 
     Inputs
     ------
-    ds : xr.Dataset
-      input netcdf to bitround
-    keepbits : int or dict
-      how many mantissa bits to keep
+    da : xr.DataArray or xr.Dataset
+      input netcdf to bitround with dtype float32
+    keepbits : int or dict of {str: int}
+      how many bits to keep. int
 
     Returns
     -------
-    ds_bitrounded : xr.Dataset
+    da_bitrounded : xr.DataArray or xr.Dataset
 
     Example
     -------
@@ -31,38 +31,22 @@ def xr_bitround(ds, keepbits):
         >>> keepbits = bp.get_keepbits(ds, info_per_bit, 0.99)
         >>> ds_bitrounded = xr_bitround(ds, keepbits)
     """
-    ds_bitrounded = ds.copy()
-    if isinstance(ds, xr.Dataset):
-        for v in ds.data_vars:
-            if (
-                ds[v].dtype == "float64"
-            ):  # fails otherwise see https://github.com/zarr-developers/numcodecs/blob/7c7dc7cc83db1ae5c9fd93ece863acedbbc8156f/numcodecs/bitround.py#L23
-                ds[v] = ds[v].astype("float32")
-            if isinstance(keepbits, int):
-                keep = keepbits
-            elif isinstance(keepbits, dict):
-                if v in keepbits.keys():
-                    keep = keepbits[v]
-                else:
-                    raise ValueError("name not for in keepbits:", keepbits.keys())
-            keep = keep - 9
-            keep = min(keep, 23)
-            keep = max(0, keep)
-            # fails for .data
-            ds_bitrounded[v].values = bitround(ds[v].values, keep)
-            ds_bitrounded[v].attrs["bitround_keepbits"] = keep + 9
-    elif isinstance(ds, xr.DataArray):
-        if isinstance(keepbits, int):
-            keep = keepbits
-        elif isinstance(keepbits, dict):
-            v = ds.name
-            if v in keepbits.keys():
-                keep = keepbits[v]
-            else:
-                raise ValueError("name not for in keepbits:", keepbits.keys())
-        keep = keep - 9
-        keep = min(keep, 23)
-        keep = max(0, keep)
-        ds_bitrounded.values = bitround(ds.values, keep)
-        ds_bitrounded.attrs["bitround_keepbits"] = keep + 9
-    return ds_bitrounded
+    if isinstance(da, xr.Dataset):
+        return da.map(xr_bitround, keepbits)
+
+    assert da.dtype == "float32"
+    da_bitrounded = da.copy()
+    if isinstance(keepbits, int):
+        keep = keepbits
+    elif isinstance(keepbits, dict):
+        v = da.name
+        if v in keepbits.keys():
+            keep = keepbits[v]
+        else:
+            raise ValueError(f"name {v} not for in keepbits: {keepbits.keys()}")
+    # fails for .data
+    da_bitrounded.values = bitround(
+        da.values, keep - 9
+    )  # uses keep mantissa bits: [0-23]
+    da_bitrounded.attrs["bitround_keepbits"] = keep  # document keepbits
+    return da_bitrounded
