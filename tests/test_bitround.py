bde148ea224bc897cfbac62a280078b833a49b25
AS <aaron.spring@mpimet.mpg.de>
2022-04-07 12:13:48 +0200

    skip dask tests

--- tests/test_bitround.py
+++ tests/test_bitround.py
@@ -42,7 +42,10 @@ def test_xr_bitround(air_temperature, input_type, implementation, keepbits, dask
             check(ds[v], ds_bitrounded[v])
 
 
-@pytest.mark.parametrize("dask", [True, False])
+@pytest.mark.parametrize(
+    "dask",
+    [pytest.param(True, marks=pytest.mark.skip(reason="require .values")), False],
+)
 @pytest.mark.parametrize("implementation", ["xarray", "julia"])
 def test_xr_bitround_dask(air_temperature, implementation, dask):
     """Test xr_bitround keeps dask."""
@@ -54,7 +57,6 @@ def test_xr_bitround_dask(air_temperature, implementation, dask):
 
     bitround = bp.xr_bitround if implementation == "xarray" else bp.jl_bitround
     ds_bitrounded = bitround(ds, keepbits)
-
     assert is_dask_collection(ds_bitrounded) == dask
 
 
