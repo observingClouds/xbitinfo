6ea6becf7474a9a199790fc34513fd7d33e31cea
Aaron Spring <aaronspring@users.noreply.github.com>
2022-04-07 23:06:00 +0200

    Update bitround.py

--- bitinformation_pipeline/bitround.py
+++ bitinformation_pipeline/bitround.py
@@ -34,12 +34,12 @@ def xr_bitround(da, keepbits):
         >>> ds_bitrounded = bp.xr_bitround(ds, keepbits)
     """
     if isinstance(da, xr.Dataset):
+        da_bitrounded = da.copy()
         for v in da.data_vars:
-            da[v] = xr_bitround(da[v], keepbits)
+            da_bitrounded[v] = xr_bitround(da_bitrounded[v], keepbits)
         return da
 
     assert da.dtype == "float32"
-    da_bitrounded = da.copy()
     if isinstance(keepbits, int):
         keep = keepbits
     elif isinstance(keepbits, dict):
@@ -49,9 +49,9 @@ def xr_bitround(da, keepbits):
         else:
             raise ValueError(f"name {v} not for in keepbits: {keepbits.keys()}")
     # fails for .data
-    da_bitrounded.values = _bitround(da.values, keep)
-    da_bitrounded.attrs["_QuantizeBitRoundNumberOfSignificantDigits"] = keep
-    return da_bitrounded
+    da.values = _bitround(da.values, keep)
+    da.attrs["_QuantizeBitRoundNumberOfSignificantDigits"] = keep
+    return da
 
 
 def jl_bitround(da, keepbits):
@@ -76,11 +76,11 @@ def jl_bitround(da, keepbits):
         >>> ds_bitrounded = bp.jl_bitround(ds, keepbits)
     """
     if isinstance(da, xr.Dataset):
+        da_bitrounded = da.copy()
         for v in da.data_vars:
-            da[v] = jl_bitround(da[v], keepbits)
-        return da
+            da_bitrounded[v] = jl_bitround(da_bitrounded[v], keepbits)
+        return da_bitrounded
 
-    da_bitrounded = da.copy()
     if isinstance(keepbits, int):
         keep = keepbits
     elif isinstance(keepbits, dict):
@@ -90,8 +90,8 @@ def jl_bitround(da, keepbits):
         else:
             raise ValueError(f"name {v} not for in keepbits: {keepbits.keys()}")
     # fails for .data
-    da_bitrounded.values = _jl_bitround(da.values, keep)
-    da_bitrounded.attrs[
+    da.values = _jl_bitround(da.values, keep)
+    da.attrs[
         "_QuantizeBitRoundNumberOfSignificantDigits"
-    ] = keep  # document keepbits
-    return da_bitrounded
+    ] = keep
+    return da
