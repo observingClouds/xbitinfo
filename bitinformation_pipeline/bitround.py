eb108b41edc79628cbe0a69c7d7fdc4c71965a8f
Aaron Spring <aaronspring@users.noreply.github.com>
2022-04-11 17:31:38 +0200

    map_blocks=False keyword added

--- bitinformation_pipeline/bitround.py
+++ bitinformation_pipeline/bitround.py
@@ -12,7 +12,7 @@ def _xr_bitround(data, keepbits):
     return codec.decode(encoded)
 
 
-def xr_bitround(da, keepbits):
+def xr_bitround(da, keepbits, map_blocks=False):
     """Apply bitrounding based on keepbits from bp.get_keepbits for xarray.Dataset or xr.DataArray wrapping numcodecs.bitround
 
     Inputs
@@ -21,6 +21,8 @@ def xr_bitround(da, keepbits):
       input netcdf to bitround with dtype float32
     keepbits : int or dict of {str: int}
       how many bits to keep. int
+    map_blocks : bool
+      if True, use xr.map_blocks, else use xr.apply_ufunc. Defaults to False.
 
     Returns
     -------
@@ -36,7 +38,7 @@ def xr_bitround(da, keepbits):
     if isinstance(da, xr.Dataset):
         da_bitrounded = da.copy()
         for v in da.data_vars:
-            da_bitrounded[v] = xr_bitround(da[v], keepbits)
+            da_bitrounded[v] = xr_bitround(da[v], keepbits, map_blocks=map_blocks)
         return da_bitrounded
 
     assert isinstance(da, xr.DataArray)
@@ -49,9 +51,9 @@ def xr_bitround(da, keepbits):
             keep = keepbits[v]
         else:
             raise ValueError(f"name {v} not for in keepbits: {keepbits.keys()}")
-    try:
+    if map_blocks:
         da = da.map_blocks(_xr_bitround, args=[keep])
-    except Exception:
+    else:
         da = xr.apply_ufunc(
             _xr_bitround, da, keep, dask="parallelized", keep_attrs=True
         )
@@ -59,7 +61,7 @@ def xr_bitround(da, keepbits):
     return da
 
 
-def jl_bitround(da, keepbits):
+def jl_bitround(da, keepbits, map_blocks=False):
     """Apply bitrounding based on keepbits from bp.get_keepbits for xarray.Dataset or xr.DataArray wrapping BitInformation.jl.round.
 
     Inputs
@@ -68,6 +70,8 @@ def jl_bitround(da, keepbits):
       input netcdf to bitround with dtype float32
     keepbits : int or dict of {str: int}
       how many bits to keep. int
+    map_blocks : bool
+      if True, use xr.map_blocks, else use xr.apply_ufunc. Defaults to False.
 
     Returns
     -------
@@ -83,7 +87,7 @@ def jl_bitround(da, keepbits):
     if isinstance(da, xr.Dataset):
         da_bitrounded = da.copy()
         for v in da.data_vars:
-            da_bitrounded[v] = jl_bitround(da[v], keepbits)
+            da_bitrounded[v] = jl_bitround(da[v], keepbits, map_blocks=map_blocks)
         return da_bitrounded
 
     if isinstance(keepbits, int):
@@ -94,9 +98,9 @@ def jl_bitround(da, keepbits):
             keep = keepbits[v]
         else:
             raise ValueError(f"name {v} not for in keepbits: {keepbits.keys()}")
-    try:
+    if map_blocks:
         da = da.map_blocks(_jl_bitround, args=[keep])
-    except Exception:
+    else:
         da = xr.apply_ufunc(
             _jl_bitround, da, keep, dask="parallelized", keep_attrs=True
         )
