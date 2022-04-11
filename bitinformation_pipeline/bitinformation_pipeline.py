c18192cd871a72aeb90f719a0e1d5e5f6cc2a01c
Aaron Spring <aaronspring@users.noreply.github.com>
2022-04-11 10:17:36 +0200

    refactor `get keepbits` (#46)
    * new get_keepbits, renamed old to _get_keepbits
    
    * rm ds from plot_bitinformation, adapt notebook
    
    * set_zero_insignificant=True by default
    
    * adapt plot_bitinformation for keepmantissabits
    
    * argmax + 1 due to julia to python conversion
    
    * add dtype test
    
    * add confidence test
    
    * fix doctest
    
    * fix tests

--- bitinformation_pipeline/bitinformation_pipeline.py
+++ bitinformation_pipeline/bitinformation_pipeline.py
@@ -19,6 +19,9 @@ jl.using("BitInformation")
 jl.eval("include(Main.path)")
 
 
+NMBITS = {64: 12, 32: 9, 16: 6}  # number of non mantissa bits for given dtype
+
+
 def get_user_input():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -114,7 +117,10 @@ def get_bitinformation(ds, dim=None, axis=None, label=None, overwrite=False, **k
                 kwargs[
                     "masked_value"
                 ] = f"convert({str(ds[var].dtype).capitalize()},NaN)"
+            if "set_zero_insignificant" not in kwargs:
+                kwargs["set_zero_insignificant"] = True
             kwargs_str = ", ".join([f"{k}={v}" for k, v in kwargs.items()])
+            # convert python to julia bool
             kwargs_str = kwargs_str.replace("True", "true").replace("False", "false")
             logging.debug(f"get_bitinformation(X, dim={dim}, {kwargs_str})")
             info_per_bit[var] = jl.eval(
@@ -140,8 +146,50 @@ def load_bitinformation(label):
         return None
 
 
-def get_keepbits(ds, info_per_bit, inflevel=0.99):
-    """Get the amount of bits to keep for a given information content.
+def get_keepbits(info_per_bit, inflevel=0.99):
+    """Get the number of mantissa bits to keep. To be used in xr_bitround and jl_bitround.
+
+    Inputs
+    ------
+    info_per_bit : dict
+      Information content of each bit for each variable in ds. This is the output from get_bitinformation.
+    inflevel : float or dict
+      Level of information that shall be preserved. Of type `float` if the
+      preserved information content should be equal across variables, otherwise of type `dict`.
+
+    Returns
+    -------
+    keepbits : dict
+      Number of mantissa bits to keep per variable
+
+    Example
+    -------
+    >>> ds = xr.tutorial.load_dataset("air_temperature")
+    >>> info_per_bit = bp.get_bitinformation(ds, dim="lon")
+    >>> bp.get_keepbits(info_per_bit)
+    {'air': 7}
+    >>> bp.get_keepbits(info_per_bit, inflevel=0.99999999)
+    {'air': 14}
+    """
+    keepmantissabits = {}
+    for v, ic in info_per_bit.items():
+        # set below threshold to zero
+        # use something a bit bigger than maximum of the last 4 bits
+        threshold = 1.5 * np.max(ic[-4:])
+        ic_over_threshold = np.where(ic < threshold, 0, ic)
+        ic_over_threshold_cum = np.cumsum(ic_over_threshold)  # CDF
+        # normed CDF
+        ic_over_threshold_cum_normed = ic_over_threshold_cum / ic_over_threshold_cum[-1]
+        # return mantissabits to keep therefore subtract sign and exponent bits
+        il = inflevel[v] if isinstance(inflevel, dict) else inflevel
+        keepmantissabits[v] = (
+            np.argmax(ic_over_threshold_cum_normed > il) + 1 - NMBITS[len(ic)]
+        )
+    return keepmantissabits
+
+
+def _get_keepbits(ds, info_per_bit, inflevel=0.99):
+    """Get the amount of mantissa bits to keep for a given information content.
 
     Inputs
     ------
@@ -156,16 +204,16 @@ def get_keepbits(ds, info_per_bit, inflevel=0.99):
     Returns
     -------
     keepbits : dict
-      Number of bits to keep per variable
+      Number of mantissa bits to keep per variable
 
     Example
     -------
     >>> ds = xr.tutorial.load_dataset("air_temperature")
     >>> info_per_bit = bp.get_bitinformation(ds, dim="lon")
-    >>> bp.get_keepbits(ds, info_per_bit)
-    {'air': 16}
-    >>> bp.get_keepbits(ds, info_per_bit, inflevel=0.99999999)
-    {'air': 23}
+    >>> bp._get_keepbits(ds, info_per_bit)
+    {'air': 7}
+    >>> bp._get_keepbits(ds, info_per_bit, inflevel=0.99999999)
+    {'air': 14}
     """
 
     def get_inflevel(var, inflevel):
@@ -185,6 +233,8 @@ def get_keepbits(ds, info_per_bit, inflevel=0.99):
         }
         Main.config = config[var]
         keepbits[var] = jl.eval("get_keepbits(config)")
+        # keep mantissa bits
+        keepbits[var] = keepbits[var] - NMBITS[len(info_per_bit[var])]
     return keepbits
 
 
@@ -195,24 +245,26 @@ def _jl_bitround(X, keepbits):
     return jl.eval("round!(X, keepbits)")
 
 
-def plot_bitinformation(ds, bitinfo):
-    """Plot bitwise information content
+def plot_bitinformation(bitinfo):
+    """Plot bitwise information content.
+
     Inputs
     ------
     bitinfo : dict
       Dictionary containing the bitwise information content for each variable
+
     Returns
     -------
     fig : matplotlib figure
-    """
 
+    """
     import cmcrameri.cm as cmc
 
     nvars = len(bitinfo)
     varnames = bitinfo.keys()
 
-    infbits_dict = get_keepbits(ds, bitinfo, 0.99)
-    infbits100_dict = get_keepbits(ds, bitinfo, 0.999999999)
+    infbits_dict = get_keepbits(bitinfo, 0.99)
+    infbits100_dict = get_keepbits(bitinfo, 0.999999999)
 
     ICnan = np.zeros((nvars, 64))
     infbits = np.zeros(nvars)
@@ -221,8 +273,9 @@ def plot_bitinformation(ds, bitinfo):
     for v, var in enumerate(varnames):
         ic = bitinfo[var]
         ICnan[v, : len(ic)] = ic
-        infbits[v] = infbits_dict[var]
-        infbits100[v] = infbits100_dict[var]
+        # infbits are all bits, infbits_dict were mantissa bits
+        infbits[v] = infbits_dict[var] + NMBITS[len(ic)]
+        infbits100[v] = infbits100_dict[var] + NMBITS[len(ic)]
     ICnan = np.where(ICnan == 0, np.nan, ICnan)
     ICcsum = np.nancumsum(ICnan, axis=1)
 
@@ -368,7 +421,7 @@ class JsonCustomEncoder(json.JSONEncoder):
 if __name__ == "__main__":
     args = get_user_input()
     ds = xr.open_mfdataset(args.filename)
-    info_per_bit = get_bitinformation(ds)
+    info_per_bit = get_bitinformation(ds, axis=0)
     print(info_per_bit)
-    keepbits = get_keepbits(ds, info_per_bit)
+    keepbits = get_keepbits(info_per_bit)
     print(keepbits)
--- tests/test_get_bitinformation.py
+++ tests/test_get_bitinformation.py
@@ -4,6 +4,7 @@
 import os
 
 import numpy as np
+import pytest
 import xarray as xr
 from numpy.testing import assert_allclose, assert_equal
 
@@ -63,10 +64,41 @@ def test_get_bitinformation_masked_value():
     bitinfo_assert_different(bitinfo, bitinfo_no_mask)
 
 
-def test_get_bitinformation_label():
+def test_get_bitinformation_set_zero_insignificant():
+    """Test bp.get_bitinformation is sensitive to set_zero_insignificant."""
+    ds = xr.tutorial.load_dataset("air_temperature")
+    dim = "lon"
+    bitinfo_szi_False = bp.get_bitinformation(ds, dim=dim, set_zero_insignificant=False)
+    bitinfo_szi_True = bp.get_bitinformation(ds, dim=dim, set_zero_insignificant=True)
+    bitinfo = bp.get_bitinformation(ds, dim=dim)
+    bitinfo_assert_different(bitinfo, bitinfo_szi_False)
+    bitinfo_assert_equal(bitinfo, bitinfo_szi_True)
+
+
+def test_get_bitinformation_confidence():
+    """Test bp.get_bitinformation is sensitive to confidence."""
+    ds = xr.tutorial.load_dataset("air_temperature")
+    dim = "lon"
+    bitinfo_conf99 = bp.get_bitinformation(ds, dim=dim, confidence=0.99)
+    bitinfo_conf50 = bp.get_bitinformation(ds, dim=dim, confidence=0.5)
+    bitinfo = bp.get_bitinformation(ds, dim=dim)
+    bitinfo_assert_different(bitinfo_conf99, bitinfo_conf50)
+    bitinfo_assert_equal(bitinfo, bitinfo_conf99)
+
+
+def test_get_bitinformation_label(rasm):
     """Test bp.get_bitinformation serializes when label given."""
-    ds = xr.tutorial.load_dataset("rasm")
+    ds = rasm
     bp.get_bitinformation(ds, dim="x", label="rasm")
     assert os.path.exists("rasm.json")
     # second call should be faster
     bp.get_bitinformation(ds, dim="x", label="rasm")
+    os.remove("rasm.json")
+
+
+@pytest.mark.parametrize("dtype", ["float64", "float32", "float16"])
+def test_get_bitinformation_dtype(rasm, dtype):
+    """Test bp.get_bitinformation returns correct number of bits depending on dtype."""
+    ds = rasm.astype(dtype)
+    v = list(ds.data_vars)[0]
+    assert len(bp.get_bitinformation(ds, dim="x")[v]) == int(dtype.replace("float", ""))
