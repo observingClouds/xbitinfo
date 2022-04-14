456bb3cbfa36e3121af1a4d705e5f0f39246f91b
Aaron Spring <aaronspring@users.noreply.github.com>
2022-04-14 13:36:45 +0200

    Update __init__.py

--- bitinformation_pipeline/__init__.py
+++ bitinformation_pipeline/__init__.py
@@ -2,5 +2,5 @@
 
 from .bitinformation_pipeline import _get_keepbits, get_bitinformation, get_keepbits
 from .bitround import jl_bitround, xr_bitround
-from .graphics import plot_bitinformation
+from .graphics import plot_bitinformation, plot_distribution
 from .save_compressed import get_compress_encoding
