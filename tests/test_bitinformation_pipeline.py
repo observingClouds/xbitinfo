0c17506664f6fc27577bce0ba86eccd930e79998
Aaron Spring <aaronspring@users.noreply.github.com>
2022-04-22 08:57:49 +0200

    Update test_bitinformation_pipeline.py

--- tests/test_bitinformation_pipeline.py
+++ tests/test_bitinformation_pipeline.py
@@ -73,11 +73,8 @@ def test_get_prefect_flow_executor(flow_paths, executor):
 def test_get_prefect_flow_inflevel_parameter(flow_paths):
     """Test get_prefect_flow runs for different parameters."""
     flow, paths = flow_paths
-    for f in paths:
-        if os.path.exists(f.replace(".nc", "_bitrounded_compressed.nc")):
-            os.remove(f.replace(".nc", "_bitrounded_compressed.nc"))
-    st090 = flow.run(parameters=dict(axis=-1, inflevel=0.90))
-    st099999999 = flow.run(parameters=dict(axis=-1, inflevel=0.99999999))
+    st090 = flow.run(parameters=dict(axis=-1, inflevel=0.90, overwrite=True))
+    st099999999 = flow.run(parameters=dict(axis=-1, inflevel=0.99999999, overwrite=True))
     keepbits = flow.get_tasks(name="get_bitinformation_keepbits")[0]
     assert (
         st099999999.result[keepbits]._result.value
