c574f69b6434155c1b9f297f2ea4d7cc667f7b39
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2022-04-25 17:36:38 +0200

    add tests for zarr compression

--- tests/test_save_compressed.py
+++ tests/test_save_compressed.py
@@ -1,7 +1,10 @@
 import os
+import shutil
 
+import numcodecs
 import pytest
 import xarray as xr
+import zarr
 
 import bitinformation_pipeline as bp
 
@@ -40,3 +43,54 @@ def test_to_compressed_netcdf_for_cdo_no_time_dim_var(air_temperature):
     ds["air_mean"] = ds["air"].isel(time=0)
     ds.to_compressed_netcdf("test.nc", for_cdo=True)
     os.remove("test.nc")
+
+
+def get_zarr_size(fn):
+    """Get size of zarr file excluding metadata"""
+    # Open file
+    grp = zarr.open_group(fn)
+    # Collect size
+    total = 0
+    for var in list(grp.keys()):
+        total += grp[var].nbytes_stored
+    return total
+
+
+def test_to_compressed_zarr(rasm):
+    """Test to_compressed_zarr reduces size on disk."""
+    ds = rasm
+    label = "file"
+    # save
+    encoding = {
+        var: {"compressor": None} for var in ds.data_vars
+    }  # deactivate default compression
+    ds.to_zarr(f"{label}.zarr", mode="w", encoding=encoding)
+    ds.to_compressed_zarr(f"{label}_compressed.zarr", mode="w")
+    # check size reduction
+    ori_size = get_zarr_size(f"{label}.zarr")
+    compressed_size = get_zarr_size(f"{label}_compressed.zarr")
+    assert compressed_size < ori_size
+    shutil.rmtree(f"{label}.zarr")
+    shutil.rmtree(f"{label}_compressed.zarr")
+
+
+def test_to_compressed_zarr_individual_compressors(eraint_uvz):
+    """Test non-default compressors."""
+    ds = eraint_uvz
+    label = "file"
+    # save
+    encoding = {
+        var: {"compressor": None} for var in ds.data_vars
+    }  # deactivate default compression
+    ds.to_zarr(f"{label}.zarr", mode="w", encoding=encoding)
+    compressors = {
+        "u": numcodecs.Blosc("zstd", clevel=7),
+        "v": numcodecs.Blosc("zlib", clevel=7, shuffle=numcodecs.Blosc.BITSHUFFLE),
+    }
+    ds.to_compressed_zarr(f"{label}_compressed.zarr", compressors, mode="w")
+    # check size reduction
+    ori_size = get_zarr_size(f"{label}.zarr")
+    compressed_size = get_zarr_size(f"{label}_compressed.zarr")
+    assert compressed_size < ori_size
+    shutil.rmtree(f"{label}.zarr")
+    shutil.rmtree(f"{label}_compressed.zarr")
