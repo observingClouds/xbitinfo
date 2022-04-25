fdc1a37c436c21475669695e4985c1817a03e7cd
AS <aaron.spring@mpimet.mpg.de>
2022-04-25 14:21:45 +0200

    add bitround_along_dim

--- bitinformation_pipeline/bitround.py
+++ bitinformation_pipeline/bitround.py
@@ -2,7 +2,7 @@ import xarray as xr
 from dask import is_dask_collection
 from numcodecs.bitround import BitRound
 
-from .bitinformation_pipeline import _jl_bitround
+from .bitinformation_pipeline import _jl_bitround, get_keepbits
 
 
 def _np_bitround(data, keepbits):
@@ -93,3 +93,54 @@ def jl_bitround(da, keepbits):
     da = xr.apply_ufunc(_jl_bitround, da, keep, dask="forbidden", keep_attrs=True)
     da.attrs["_QuantizeBitRoundNumberOfSignificantDigits"] = keep
     return da
+
+
+def bitround_along_dim(
+    ds, info_per_bit, dim, inflevels=[1.0, 0.9999, 0.99, 0.975, 0.95]
+):
+    """
+    Apply bitrounding on slices along dim based on inflevels.
+    Helper function to generate data for Fig. 3 in Klöwer et al. 2021.
+
+    Klöwer, M., Razinger, M., Dominguez, J. J., Düben, P. D., & Palmer, T. N. (2021). Compressing atmospheric data into its real information content. Nature Computational Science, 1(11), 713–724. doi: 10/gnm4jj
+
+    Inputs
+    ------
+    ds : xr.Dataset, xr.DataArray
+      input
+    info_per_bit : dict
+      Information content of each bit for each variable in ds. This is the output from get_bitinformation.
+    dim : str
+      name of dimension for slicing
+    inflevels : list of floats
+      Level of information that shall be preserved.
+
+    Returns
+    -------
+    ds : xr.Dataset, xr.DataArray
+      bitrounded on slices along dim based on inflevels
+
+    Example
+    -------
+    >>> ds = xr.tutorial.load_dataset("air_temperature")
+    >>> info_per_bit = bp.get_bitinformation(ds, dim="lon")
+    >>> ds_bitrounded_along_lon = bitround_along_dim(ds, info_per_bit, dim="lon")
+    >>> (ds - ds_bitrounded_along_lon)["air"].isel(time=0).plot()
+    """
+    stride = ds[dim].size // len(inflevels)
+    new_ds = []
+    for i, inf in enumerate(inflevels):  # last slice might be a bit larger
+        ds_slice = ds.isel(
+            {
+                dim: slice(
+                    stride * i, stride * (i + 1) if i != len(inflevels) - 1 else None
+                )
+            }
+        )
+        keepbits_slice = get_keepbits(info_per_bit, inf)
+        if inf != 1:
+            ds_slice_bitrounded = xr_bitround(ds_slice, keepbits_slice)
+        else:
+            ds_slice_bitrounded = ds_slice
+        new_ds.append(ds_slice_bitrounded)
+    return xr.concat(new_ds, dim)
