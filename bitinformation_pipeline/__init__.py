930cb8645004e9cd13f29ef931b5e90fd0953cd7
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2022-04-26 14:48:39 +0200

    Add convenience function to save compressed zarr (#71)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Aaron Spring <aaronspring@users.noreply.github.com>

--- bitinformation_pipeline/__init__.py
+++ bitinformation_pipeline/__init__.py
@@ -7,4 +7,4 @@ from .bitinformation_pipeline import (
     plot_bitinformation,
 )
 from .bitround import jl_bitround, xr_bitround
-from .save_compressed import get_compress_encoding
+from .save_compressed import get_compress_encoding_nc, get_compress_encoding_zarr
