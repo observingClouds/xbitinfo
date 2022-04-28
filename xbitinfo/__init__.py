517f43c250a77da5dff46999d9acc70890caae40
Aaron Spring <aaronspring@users.noreply.github.com>
2022-04-28 09:05:06 +0200

    Update __init__.py

--- xbitinfo/__init__.py
+++ xbitinfo/__init__.py
@@ -4,3 +4,10 @@ from .bitround import jl_bitround, xr_bitround
 from .graphics import plot_bitinformation, plot_distribution
 from .save_compressed import get_compress_encoding_nc, get_compress_encoding_zarr
 from .xbitinfo import _get_keepbits, get_bitinformation, get_keepbits, get_prefect_flow
+
+from pkg_resources import DistributionNotFound, get_distribution
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:  # pragma: no cover
+    __version__ = '0.0.0'  # pragma: no cover
