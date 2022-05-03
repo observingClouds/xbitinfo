8894c9becafa6c4d80f5d48b190f2904643f4e30
AS <aaron.spring@mpimet.mpg.de>
2022-05-03 15:46:41 +0200

    fix nb

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,7 @@ xarray.Dataset.__module__ = "xarray"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
+    "myst_nb",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.extlinks",
@@ -48,7 +49,6 @@ extensions = [
     "sphinxcontrib.napoleon",
     "sphinx.ext.viewcode",
     "sphinx_copybutton",
-    "nbsphinx",
     "IPython.sphinxext.ipython_directive",
     "IPython.sphinxext.ipython_console_highlighting",
 ]
