3d84755206eca2abe922e6db27ab8128d3d9f676
Aaron Spring <aaronspring@users.noreply.github.com>
2022-06-03 16:39:23 +0200

    remove history from long_description

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
     description="Retrieve information content and compress accordingly.",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + "\n\n" + history,
+    long_description=readme,
     include_package_data=True,
     keywords="xbitinfo",
     name="xbitinfo",
