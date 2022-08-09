d83ef84debf99fded5f708cefc98bee85a86ddd3
Aaron Spring <aaronspring@users.noreply.github.com>
2022-08-09 11:14:39 +0200

    fix prefect<2.0

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ test_requirements = ["pytest", "pytest-lazy-fixture", "pooch", "netcdf4", "dask"
 
 extras_require = {
     "viz": ["matplotlib", "cmcrameri"],
-    "prefect": ["prefect>=1.0.0"],
+    "prefect": ["prefect>=1.0.0,<2.0"],
 }
 extras_require["complete"] = sorted({v for req in extras_require.values() for v in req})
 extras_require["test"] = test_requirements
