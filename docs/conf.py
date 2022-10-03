17a4d5f4fb9750ef2fc5ad6706929ccd9d0864ee
Aaron Spring <aaronspring@users.noreply.github.com>
2022-10-03 19:02:27 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -169,7 +169,7 @@ intersphinx_mapping = {
 # Should only be uncommented when testing page development while notebooks
 # are breaking.
 # nbsphinx_kernel_name = "bitinfo-docs"  # doesnt work
-nbsphinx_allow_errors = True
+nbsphinx_allow_errors = False
 nbsphinx_timeout = 600
 nbsphinx_execute = "auto"  # "never" "always"
 jupyter_execute_notebooks = "auto"
