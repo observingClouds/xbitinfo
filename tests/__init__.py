cc4a721c16db257b26fd1a5673bbf53286df3b42
Aaron Spring <aaronspring@users.noreply.github.com>
2022-10-23 15:04:55 +0200

    requires_julia tests/__init__

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,15 @@
 """Unit test package for xbitinfo."""
+
+def _importorskip(modname, minversion=None):
+    try:
+        mod = importlib.import_module(modname)
+        has = True
+        if minversion is not None:
+            if LooseVersion(mod.__version__) < LooseVersion(minversion):
+                raise ImportError("Minimum version not satisfied")
+    except ImportError:
+        has = False
+    func = pytest.mark.skipif(not has, reason=f"requires {modname}")
+    return has, func
+  
+has_julia, requires_julia = _importorskip("julia")
