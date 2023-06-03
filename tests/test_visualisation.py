5585024acbf5f22b96bda06f7b1910b8c3881a56
Ishaanj18 <93217075+Ishaanj18@users.noreply.github.com>
2023-06-04 00:54:54 +0530

    added test for add_bitinfo_labels

---
+++ tests/test_visualisation.py
@@ -0,0 +1,37 @@
+import xarray as xr
+import xbitinfo as xb
+import pytest
+import matplotlib.pyplot as plt 
+from xbitinfo.graphics import add_bitinfo_labels
+
+def test_add_bitinfo_labels():
+    ds = xr.tutorial.load_dataset("air_temperature")
+    info_per_bit = xb.get_bitinformation(ds, dim="lon")
+    inflevels = [1.0, 0.9999, 0.99, 0.975, 0.95]
+    keepbits = [6, 4, 3, 8, 5]
+    ds_bitrounded_along_lon = xb.bitround.bitround_along_dim(ds, info_per_bit, dim="lon", inflevels=inflevels)
+    diff = (ds - ds_bitrounded_along_lon)["air"].isel(time=0)
+    diff.plot()
+    ax=plt.gca()
+
+    add_bitinfo_labels(diff, info_per_bit, inflevels, keepbits)
+
+    # Check if a Matplotlib figure object is created
+    assert plt.gcf() is not None
+
+    # Check if the text labels were added to the plot
+    assert len(ax.texts) == 2 * len(keepbits)
+
+    # Check if the plot contains the expected number of lines
+    assert len(ax.lines) == len(keepbits)
+
+    # Check if the labels have the correct content
+    expected_inflevels = ['98.89%', '86.75%', '73.44%', '99.97%', '95.28%']
+    for i, keep in enumerate(keepbits):
+        inf_text = expected_inflevels[i]
+        keepbits_text = f"keepbits = {keep}"
+        assert ax.texts[i*2].get_text() == inf_text
+        assert ax.texts[i*2 + 1].get_text() == keepbits_text
+
+    # Cleanup the plot
+    plt.close()
